d3bedd4f5538563e2c15a71371ae5ff338f039c5
AlxAshl <97846834+AlxAshl@users.noreply.github.com>
2022-09-02 22:19:18 +0300

    TEST-COMMIT

--- project/src/components/comment-form/comment-form.tsx
+++ project/src/components/comment-form/comment-form.tsx
@@ -30,7 +30,7 @@ function CommentForm(props: CommentFormProps): JSX.Element {
     dispatch(fetchCommentAction());
   },[updateComment, dispatch]);
 
-  const formChangeHandler = (evt: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
+  const handleFormChange = (evt: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
     const {name, value} = evt.target;
     setFormData({...formData, [name]: value,});
   };
@@ -49,32 +49,32 @@ function CommentForm(props: CommentFormProps): JSX.Element {
           ? <div>Updating...</div>
           :
           <>
-            <input onChange={formChangeHandler} className="form__rating-input visually-hidden" name="rating" value={5} id="5-stars" type="radio" />
+            <input onChange={handleFormChange} className="form__rating-input visually-hidden" name="rating" value={5} id="5-stars" type="radio" />
             <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
               <svg className="form__star-image" width="37" height="33">
                 <use xlinkHref="#icon-star"></use>
               </svg>
             </label>
-            <input onChange={formChangeHandler} className="form__rating-input visually-hidden" name="rating" value={4} id="4-stars" type="radio" />
+            <input onChange={handleFormChange} className="form__rating-input visually-hidden" name="rating" value={4} id="4-stars" type="radio" />
             <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
               <svg className="form__star-image" width="37" height="33">
                 <use xlinkHref="#icon-star"></use>
               </svg>
             </label>
-            <input onChange={formChangeHandler} className="form__rating-input visually-hidden" name="rating" value={3} id="3-stars" type="radio" />
+            <input onChange={handleFormChange} className="form__rating-input visually-hidden" name="rating" value={3} id="3-stars" type="radio" />
             <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
               <svg className="form__star-image" width="37" height="33">
                 <use xlinkHref="#icon-star"></use>
               </svg>
             </label>
-            <input onChange={formChangeHandler} className="form__rating-input visually-hidden" name="rating" value={2} id="2-stars" type="radio" />
+            <input onChange={handleFormChange} className="form__rating-input visually-hidden" name="rating" value={2} id="2-stars" type="radio" />
             <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
               <svg className="form__star-image" width="37" height="33">
                 <use xlinkHref="#icon-star"></use>
               </svg>
             </label>
 
-            <input required onChange={formChangeHandler} className="form__rating-input visually-hidden" name="rating" value={1} id="1-star" type="radio" />
+            <input required onChange={handleFormChange} className="form__rating-input visually-hidden" name="rating" value={1} id="1-star" type="radio" />
             <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
               <svg className="form__star-image" width="37" height="33">
                 <use xlinkHref="#icon-star"></use>
@@ -84,7 +84,7 @@ function CommentForm(props: CommentFormProps): JSX.Element {
       </div>
       {!updateComment
         ? <div className="reviews__textarea form__textarea"></div>
-        : <textarea required maxLength={300} minLength={50} onChange={formChangeHandler} className="reviews__textarea form__textarea" id="review" name="comment" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>}
+        : <textarea required maxLength={300} minLength={50} onChange={handleFormChange} className="reviews__textarea form__textarea" id="review" name="comment" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>}
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
